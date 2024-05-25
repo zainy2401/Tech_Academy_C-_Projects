049f1470181761f8a68dd277abf9a205a9515e12
zainy2401 <149741485+zainy2401@users.noreply.github.com>
2024-05-25 01:34:50 +0100

    Add files via upload

---
+++ MethodClassAssignment/MethodClassAssignment/Operations.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodClassAssignment
+{
+    public class Operations
+    {
+        //Creates a method that does not have to return a value, but takes in 2 integer variables
+        public void addition(int num1, int num2)
+        {
+            Console.WriteLine(num1 + " + 5 = " + (num1+5)); //Outputs to the console the calculation of the first integer passed added by 5
+            Console.WriteLine("The second integer passed is: " + num2); //Outputs to the console the second integer passed
+        }
+    }
+}
---
+++ MethodClassAssignment/MethodClassAssignment/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MethodClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operations operation = new Operations(); //Instantiates (creates an object of the created class "operation")
+            operation.addition(5,10); //Passes 2 integers in the method called via the new object created
+        }
+    }
+}
