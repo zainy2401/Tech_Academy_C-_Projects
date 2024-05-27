d0efe8a46c7cc0678787a32b5fc2592178d3f7eb
zainy2401 <149741485+zainy2401@users.noreply.github.com>
2024-05-27 20:46:47 +0100

    Implemented proper of type interface

--- PolymorphismAssignment/PolymorphismAssignment/Program.cs
+++ PolymorphismAssignment/PolymorphismAssignment/Program.cs
@@ -6,8 +6,7 @@ namespace PolymorphismAssignment
     {
         static void Main(string[] args)
         {
-            Employee person = new Employee() { Id = 1, firstName = "Zain", lastName = "Aziz" }; //This instantiates an object of the employee class, defines the data in the curly braces
-            person.SayName(); //Calls the say name method on the person object
+            IQuittable person = new Employee() { Id = 1, firstName = "Zain", lastName = "Aziz" }; //Creates an object of IQuittable interface type on the Employee class
             person.Quit(); //Calls the quit method on the person object
         }
     }
