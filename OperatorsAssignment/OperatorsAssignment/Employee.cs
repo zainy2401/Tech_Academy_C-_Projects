b075e205d93abbbcfefb78ff037a52bcc2ed7728
zainy2401 <149741485+zainy2401@users.noreply.github.com>
2024-06-06 20:07:38 +0100

    Add files via upload

---
+++ OperatorsAssignment/OperatorsAssignment/Employee.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OperatorsAssignment
+{
+    class Employee
+    {
+        public int Id { get; set; } //Deinfes an integer variable
+        public string FirstName { get; set; } //Defines a string variable
+        public string LastName { get; set; } //Defines a string variable
+
+        //This method overloads an the equals operator to compare 2 objects of the Employee class on the basis of the Id property and returns a boolean value along with outputing a message
+        public static bool operator ==(Employee employee1, Employee employee2)
+        {
+            if(employee1.Id == employee2.Id)
+            {
+                Console.WriteLine("The objects match");
+            }
+            else
+            {
+                Console.WriteLine("The objects DO NOT match");
+            }
+            return employee1.Id == employee2.Id;
+        }
+
+        //This method does the same as above but instead compares if they are not equal on the basis of the Id property
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            if (employee1.Id == employee2.Id)
+            {
+                Console.WriteLine("The objects DO NOT match");
+            }
+            else
+            {
+                Console.WriteLine("The objects match");
+            }
+            return employee1.Id != employee2.Id;
+        }
+    }
+}
---
+++ OperatorsAssignment/OperatorsAssignment/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace OperatorsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee person1 = new Employee() { Id = 1}; //Creates and initalises object with id of 1
+            Employee person2 = new Employee() { Id = 2 }; //Creates and initalises object with id of 2
+            Employee person3 = new Employee() { Id = 1}; //Creates and initalises object with id of 1 which is a duplicate of the first object
+            Console.WriteLine(person1 == person2); //Compares the first object with second, should output false
+            Console.WriteLine();
+            Console.WriteLine(person1 == person3); //Compares the first object with third, should output true
+        }
+    }
+}
